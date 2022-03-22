38968e7a272a7c8ba8788fee777c1feb30dca13a
RommelAquinoJr <100814020+RommelAquinoJr@users.noreply.github.com>
2022-03-21 20:35:28 -0700

    Update home.py

--- home.py
+++ home.py
@@ -1,2 +1,17 @@
-def home():
+from flask import Flask
+from flask import escape
 
+myapp_obj = Flask(__name__)
+
+@myapp_obj.route("/")
+
+def home():
+  name = {'name': 'Lisa'}
+  return '''
+  <html>
+  <body>
+    <h1>Welcome ''' + name['name'] + '''!</h1>
+  </body>
+  </html>'''
+
+myapp_obj.run()
