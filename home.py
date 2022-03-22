d4a7a64474b878c124b096e53ad79563fccefb25
RommelAquinoJr <100814020+RommelAquinoJr@users.noreply.github.com>
2022-03-21 20:48:12 -0700

    Update home.py

--- home.py
+++ home.py
@@ -9,9 +9,10 @@ def home():
   name = {'name': 'Lisa'}
   return '''
   <html>
-  <body>
-    <h1>Welcome ''' + name['name'] + '''!</h1>
-  </body>
+    <body>
+      <h1>Welcome ''' + name['name'] + '''!</h1>
+      <a href="www.google.com">not google</a>
+    </body>
   </html>'''
 
 myapp_obj.run()
